e1c61766914f436685afa5f4a20eda0d6ec9dd7f
Andrew Qiu <git@andrewqiu.com>
2025-06-11 16:50:07 +1200

    ✨ create test route lol

---
+++ app/testing/page.tsx
@@ -0,0 +1,13 @@
+import RootLayout from "../layout";
+
+export default function Testing() {
+  return(
+      <RootLayout>
+        <div className="grid place-items-center min-h-screen">
+            <div className="flex flex-col gap-4">
+                <h1>agfc goated</h1>
+            </div>
+        </div>
+      </RootLayout>
+      )
+}
