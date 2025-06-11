1fdd99d025c776a5e587bbc3c200008dae2a03b5
Andrew Qiu <git@andrewqiu.com>
2025-06-11 17:32:40 +1200

    nav component setup (base)

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -4,8 +4,9 @@ export default function Testing() {
   return(
       <RootLayout>
         <div className="grid place-items-center min-h-screen">
-            <div className="flex flex-col gap-4">
-                <h1>agfc goated</h1>
+            <div className="flex flex-col tracking-tight">
+                <h1 className="font-bold text-3xl">agfc goated</h1>
+                <h2 className="font-semibold text-3xl">lorem ipsum dolor sit amet, sdhaidgfsjchzdbxkjcngzdjf</h2>
             </div>
         </div>
       </RootLayout>
---
+++ src/components/navigation/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function Navbar(){
+  return(
+    <nav className="py-4 px-32 flex justify-between">
+      <div>
+        dsadsad
+      </div>
+      <div>
+        Button here
+      </div>
+    </nav>
+  )
+}
