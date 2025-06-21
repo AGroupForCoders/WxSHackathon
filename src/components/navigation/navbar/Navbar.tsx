c810b4d2f3ae69d8dde00129f3b6f77dd45bb2d9
Andrew Qiu <git@andrewqiu.com>
2025-06-22 03:31:03 +1200

    💩 mobile navbar v0.5 (cooked)

--- src/components/navigation/navbar/Navbar.tsx
+++ src/components/navigation/navbar/Navbar.tsx
@@ -1,27 +1,33 @@
 import Link from 'next/link';
 import {Button} from "@/components/primitives/Button";
+import NavbarMobile from "@/components/navigation/navbar/NavbarMobile";
 
 export default function Navbar(){
 
   const navLinkStyles = "font-semibold transition hover:opacity-50"
 
   return(
-    <nav className="py-4 px-32 flex justify-between absolute top-0 w-full">
+    <nav className="py-4 px-8 md:px-32 flex justify-between absolute top-0 w-full">
       <div className="flex gap-8 items-center">
         <Link href="/" className="bg-accent-wdcc h-full w-[45px] rounded-full grid place-items-center">
           {/* PLACEHOLDER FOR LOGO OF SOME KIND */}
           <span className="text-white text-xs">logo?</span>
         </Link>
-        <Link href="/" className={navLinkStyles}>About</Link>
-        <Link href="/" className={navLinkStyles}>Sponsors</Link>
-        <Link href="/" className={navLinkStyles}>FAQ</Link>
-        <Link href="/gallery" className={navLinkStyles}>Gallery</Link>
-        <Link href="/themes" className={navLinkStyles}>Past themes</Link>
-        <Link href="/winners" className={navLinkStyles}>Past winners</Link>
-        <Link href="/organisers" className={navLinkStyles}>Organisers</Link>
+        <div className="hidden md:flex gap-8 items-center">
+          <Link href="/" className={navLinkStyles}>About</Link>
+          <Link href="/" className={navLinkStyles}>Sponsors</Link>
+          <Link href="/" className={navLinkStyles}>FAQ</Link>
+          <Link href="/gallery" className={navLinkStyles}>Gallery</Link>
+          <Link href="/themes" className={navLinkStyles}>Past themes</Link>
+          <Link href="/winners" className={navLinkStyles}>Past winners</Link>
+          <Link href="/organisers" className={navLinkStyles}>Organisers</Link>
+        </div>
       </div>
       <div>
-        <Button variant={{style: "secondary"}} href="https://www.youtube.com/watch?v=p7YXXieghto">Registration {"->"}</Button>
+        <NavbarMobile
+          className="md:hidden"
+        />
+        <Button className="hidden md:flex" variant={{style: "secondary"}} href="https://www.youtube.com/watch?v=p7YXXieghto">Registration {"->"}</Button>
       </div>
     </nav>
   )
