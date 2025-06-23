afa44773195834d0daac9f21bd635818865f75e1
Andrew Qiu <git@andrewqiu.com>
2025-06-23 18:46:19 +1200

    💄 linting + page defs

--- app/(home)/_components/visuals/Skyline.tsx
+++ app/(home)/_components/visuals/Skyline.tsx
@@ -1,9 +1,7 @@
-import { animate, onScroll } from "animejs";
-
 export default function Skyline(){
   return(
     <div>
-
+      {/* Placeholder */}
     </div>
   )
 }
---
+++ app/winners/page.tsx
@@ -0,0 +1,9 @@
+export default function PastWinnersPage() {
+  return(
+    <main className="min-h-screen">
+      <div className="my-24 h-128">
+        past winners.
+      </div>
+    </main>
+  )
+}
