381adaf3a383193e7cdb188b9b659f66e8c9a3fb
jebadenas <jbad180@aucklanduni.ac.nz>
2025-07-01 10:56:56 +1200

    🫦 past winners data

---
+++ app/winners/past-winners/pastWinners2024.ts
@@ -0,0 +1,97 @@
+import { PastWinners } from '@/types/WinnerType';
+
+export const pastWinners2024: PastWinners = {
+  year: 2024,
+  orderedWinners: [
+    {
+      place: '1st Place',
+      teamName: 'Git Kermit',
+      image: '/winners/2024/2024-1st copy.jpg',
+      members: [
+        'Cory Brown',
+        'Kevin Cao',
+        'Shuhei Yokkaichi',
+        'Samuel Chung',
+        'Jessica Zeng',
+        'Chris Oliver',
+      ],
+      description:
+        "Introducing Lunar Luck! Built using Node.js, Express.js, and enhanced by custom artwork by Jessica, Lunar Luck features slots, alien racing, and chance games. The premise is that a serious divide emerges between the rich and poor after Earth's destruction and oxygen becomes scarce. Our service offers the poor a chance to gamble for more oxygen – with the risk of losing it all.",
+      github: 'https://github.com/cbro223/SESAHackthon2024',
+    },
+    {
+      place: '2nd Place',
+      teamName: 'Java Skunk',
+      image: '/winners/2024/2024-2nd copy.jpg',
+      members: [
+        'Jason Huang',
+        'Chris Valenzuela',
+        'Kinzi Ceolin',
+        'Siyeon Kim',
+        'Xin Yue',
+        'Skyler Solomon-Gavin',
+      ],
+      description:
+        'A gamified learning platform to educate the youth about what life was like back on Earth. Players can learn about Earth through exploring various different regions and collect animal cards in the process. Animal cards can be used to battle against other players in a card game format.',
+      github: 'https://github.com/Bobsters6666/SpaceZoo',
+    },
+    {
+      place: '3rd Place',
+      teamName: 'Kappachungus',
+      image: '/winners/2024/2024-3rd copy.jpg',
+      members: [
+        'Ryan Yoon',
+        'Joshua Morley',
+        'Ray Xiao',
+        'Katie Zhou',
+        'Connor Williams',
+        'Jaap Skinner',
+      ],
+    },
+  ],
+  specialAwards: [
+    {
+      place: 'Most Overengineered',
+      teamName: 'Exception Handlers',
+      image: '/winners/2024/2024-4th copy.jpg',
+      members: [
+        'Aref Osman',
+        'Alex Brown',
+        'Brandon Seng Han Chan',
+        'Daniel Munn',
+        'Henry Ly',
+        'James Tasker',
+      ],
+      description:
+        "A Jackbox-esque multiplayer spaceship crew simulator game: 3 players take on the roles of a Captain, an Engineer and a Chemist to survive in deep space for as long as they can. Each player must work through a series of mini-games, and must communicate and collaborate with each other to obtain resources essential for the ship's survival. Three mobile web clients and a TV lobby screen connect to a server to seamlessly manage game state and display collective real time updates.",
+      github: 'https://github.com/ExceptionHandlersUOA',
+      deployedLink: 'https://space.feroxfoxxo.com/',
+    },
+    {
+      place: 'Best Design',
+      teamName: 'Git Kermit',
+      image: '/winners/2024/2024-5th copy.jpg',
+      members: [
+        'Cory Brown',
+        'Kevin Cao',
+        'Shuhei Yokkaichi',
+        'Samuel Chung',
+        'Jessica Zeng',
+        'Chris Oliver',
+      ],
+    },
+    {
+      place: 'Most Entertaining',
+      teamName: 'print(“hello world”)',
+      image: '/winners/2024/2024-6th copy.jpg',
+      members: [
+        'Andre Camerino',
+        'Evan Au',
+        'Luase Fontaine',
+        'Ashlee Shum',
+        'Gabrielle Esguerra',
+        'Jayden Newport',
+      ],
+    },
+  ],
+};
