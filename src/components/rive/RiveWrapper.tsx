1088aa8fcd4563a3971c861b878bf91521948caa
Andrew Qiu <git@andrewqiu.com>
2025-07-01 03:12:35 +1200

    💩 import Rive components directly from WDCC4 lmao

---
+++ src/components/rive/RiveWrapper.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useRive } from "@rive-app/react-canvas";
+
+// Wrapper component to isolate useRive logic that
+// renders the <RiveComponent />
+function RiveWrapper({ src, isPlaying }: { src: string; isPlaying: boolean }) {
+    const { rive, RiveComponent } = useRive({
+        src: src,
+        artboard: "Artboard",
+        autoplay: true,
+    });
+
+    React.useEffect(() => {
+        if (rive) {
+            if (isPlaying) rive.play();
+            else rive.pause();
+        }
+    }, [isPlaying, rive]);
+
+    return <RiveComponent />;
+}
+
+export default RiveWrapper;
