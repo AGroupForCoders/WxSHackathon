670b3ffaeddc33f0d6f5faeab9363021a046da4c
Andrew Qiu <git@andrewqiu.com>
2025-07-02 07:46:28 +1000

    ✨ add remaining sub-hero sections

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,12 +2,16 @@ import Faqs from "./_components/Faqs";
 import Hero from "./_components/Hero";
 import Opportunity from "./_components/Opportunity";
 import Sponsors from "./_components/Sponsors";
+import AboutStats from "./_components/visuals/AboutStats";
+import About from "./_components/visuals/About";
 
 export default function Home() {
   return (
     <main className="min-h-screen responsive-body">
       <Hero />
-      <div className="gap-32 flex flex-col">
+      <div className="gap-32 flex flex-col mt-[-800px]">
+        <AboutStats/>
+        <About/>
         <Opportunity />
         <Faqs />
         <Sponsors />
