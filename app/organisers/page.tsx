63cc48631c79535f407213f89500e3288d7a8a74
AntGa <antonvince.garay@gmail.com>
2025-07-04 18:32:42 +1200

    ✨♻ added tailwind variants & refactored card component

--- app/organisers/page.tsx
+++ app/organisers/page.tsx
@@ -5,10 +5,14 @@ export default function OrganisersPage() {
       <div className="my-24 ">
         <h1 className="text-center font-bold text-6xl">Organisers</h1>
         <div className="w-full relative h-[1000px]">
-          <OrganisersCard />
+          <OrganisersCard className="bottom-0 left-0 " />
+          <OrganisersCard className="bottom-0 right-0" backgroundColor="sesa" />
+          <OrganisersCard className="left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+          <OrganisersCard className="left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+          <OrganisersCard className="bottom-0 left-0 " />
+          <OrganisersCard className="bottom-0 left-0 " />
         </div>
       </div>
     </main>
   );
 }
-
--- src/(features)/organisers/components/OrganisersCard.tsx
+++ src/(features)/organisers/components/OrganisersCard.tsx
@@ -1,11 +1,46 @@
 import { HTMLAttributes } from "react";
 import { cn } from "@/utils/cn";
+import { tv, VariantProps } from "tailwind-variants";
 
-interface OrganisersCardProps extends HTMLAttributes<HTMLDivElement> {
-}
+const organiserscard = tv({
+  base: "w-full h-full rounded-4xl border-4 border-abbey-apricot-02",
+  variants: {
+    backgroundColor: {
+      wdcc: "bg-wdcc-blue-50",
+      sesa: "bg-sesa-teal-50",
+      //to do make gradient
+      wdccsesa: "",
+    },
+  },
+  defaultVariants: {
+    backgroundColor: "wdcc",
+  },
+});
 
-export function OrganisersCard({className, children, ...props} : OrganisersCardProps) {
+interface OrganisersCardProps
+  extends VariantProps<typeof organiserscard>,
+    HTMLAttributes<HTMLDivElement> {}
+
+export function OrganisersCard({
+  className,
+  children,
+  backgroundColor,
+  ...props
+}: OrganisersCardProps) {
   return (
-    <div className={cn('w-[400px] h-[500px] bg-white border-14 border-teal-500 rounded-[30px] shadow-lg', className)} />
+    <div
+      className={cn(
+        "w-[350px] h-[450px] bg-white rounded-[30px] absolute",
+        className
+      )}
+      {...props}
+    >
+      <div className="relative w-full h-full">
+        <div
+          className={organiserscard({ backgroundColor: backgroundColor })}
+        ></div>
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[280px] h-[380px] bg-white rounded-4xl"></div>
+      </div>
+    </div>
   );
 }
