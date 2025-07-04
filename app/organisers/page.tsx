61a5a486f7ca5aca4207715499a90e991351cf30
mattrenren <mren843@aucklanduni.ac.nz>
2025-07-04 18:09:19 +1200

    ✨Add in card layout

--- app/organisers/page.tsx
+++ app/organisers/page.tsx
@@ -5,10 +5,14 @@ export default function OrganisersPage() {
       <div className="my-24 ">
         <h1 className="text-center font-bold text-6xl">Organisers</h1>
         <div className="w-full relative h-[1000px]">
-          <OrganisersCard />
+          <OrganisersCard className="bottom-0 left-1/2 -translate-x-1/2 z-50" />
+          <OrganisersCard className="bottom-95 left-1/2 -translate-x-1/2 z-40" />
+          <OrganisersCard className="right-8 top-15 rotate-[4.5deg] z-30" />
+          <OrganisersCard className="right-5 bottom-30 rotate-[9deg] z-40" />
+          <OrganisersCard className="left-8 top-15 -rotate-[4.5deg] z-30" />
+          <OrganisersCard className="left-5 bottom-30 -rotate-[9deg] z-40" />
         </div>
       </div>
     </main>
   );
 }
-
--- src/(features)/organisers/components/OrganisersCard.tsx
+++ src/(features)/organisers/components/OrganisersCard.tsx
@@ -1,11 +1,20 @@
 import { HTMLAttributes } from "react";
 import { cn } from "@/utils/cn";
 
-interface OrganisersCardProps extends HTMLAttributes<HTMLDivElement> {
-}
+interface OrganisersCardProps extends HTMLAttributes<HTMLDivElement> {}
 
-export function OrganisersCard({className, children, ...props} : OrganisersCardProps) {
+export function OrganisersCard({
+  className,
+  children,
+  ...props
+}: OrganisersCardProps) {
   return (
-    <div className={cn('w-[400px] h-[500px] bg-white border-14 border-teal-500 rounded-[30px] shadow-lg', className)} />
+    <div
+      className={cn(
+        "w-[350px] h-[450px] bg-white border-27 border-teal-500 rounded-[20px] shadow-lg absolute",
+        className
+      )}
+      {...props}
+    />
   );
 }
