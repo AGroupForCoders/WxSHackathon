9069b99248ee804abb2ae565a323777d190e7755
jebadenas <jbad180@aucklanduni.ac.nz>
2025-07-04 21:32:03 +1200

    sponsors (not finalised)

---
+++ src/(features)/hero/sponsors/data/foodSponsors.ts
@@ -0,0 +1,53 @@
+import { Sponsor } from "@/(features)/winners/types/SponsorType";
+
+export const foodSponsors: Sponsor[] = [
+  {
+    name: "Indomie",
+    type: "food",
+    websiteLink: "",
+    image: "/sponsors/indomie.png",
+    imageAlt: "atlassian logo",
+  },
+  {
+    name: "Red Bull",
+    type: "food",
+    websiteLink: "",
+    image: "/sponsors/red-bull.png",
+    imageAlt: "atlassian logo",
+  },
+  {
+    name: "Indomie",
+    type: "food",
+    websiteLink: "",
+    image: "/sponsors/indomie.png",
+    imageAlt: "atlassian logo",
+  },
+  {
+    name: "Indomie",
+    type: "food",
+    websiteLink: "",
+    image: "/sponsors/indomie.png",
+    imageAlt: "atlassian logo",
+  },
+  {
+    name: "Indomie",
+    type: "food",
+    websiteLink: "",
+    image: "/sponsors/indomie.png",
+    imageAlt: "atlassian logo",
+  },
+  {
+    name: "Indomie",
+    type: "food",
+    websiteLink: "",
+    image: "/sponsors/indomie.png",
+    imageAlt: "atlassian logo",
+  },
+  {
+    name: "Indomie",
+    type: "food",
+    websiteLink: "",
+    image: "/sponsors/indomie.png",
+    imageAlt: "atlassian logo",
+  },
+];
