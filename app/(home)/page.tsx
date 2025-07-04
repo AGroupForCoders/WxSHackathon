ee8da1efac17651ead3eb6fcda0b1338644814fe
Andrew Qiu <git@andrewqiu.com>
2025-07-05 03:22:29 +1000

    📝 index layout reorganisation

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,17 +3,25 @@ import { Hero } from "@/(features)/hero";
 import { Opportunity } from "@/(features)/hero/opportunity";
 import { Sponsors } from "@/(features)/hero/sponsors";
 import { AboutStats, About } from "@/(features)/hero";
+import AboutGallery from "@/(features)/hero/components/visuals/AboutGallery";
 
 export default function Home() {
   return (
-    <main className="min-h-screen responsive-body">
+    <main className="min-h-screen">
       <Hero />
-      <div className="gap-32 flex flex-col mt-[-800px]">
+      <div className="gap-32 flex flex-col mt-[-800px] mb-32 overflow-hidden">
         <AboutStats />
-        <About />
-        <Opportunity />
-        <FaqsSection />
-        <Sponsors />
+        <div className="responsive-body">
+          <About/>
+        </div>
+        <AboutGallery />
+        <div className="responsive-body">
+          <div className="gap-32 flex flex-col responsive-body">
+            <Opportunity />
+            <FaqsSection />
+            <Sponsors />
+          </div>
+        </div>
       </div>
     </main>
   );
