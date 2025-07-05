e339ed79833b5f118deb38e54760c2ed50e8d886
jebadenas <jbad180@aucklanduni.ac.nz>
2025-07-05 19:06:04 +1200

    added vertical gap between see more cards

--- src/(features)/hero/more/components/SeeMore.tsx
+++ src/(features)/hero/more/components/SeeMore.tsx
@@ -5,7 +5,7 @@ export function SeeMore() {
   return (
     <section className="flex flex-col  items-center justify-center w-full gap-16">
       <h1 className="font-bold text-4xl">See more</h1>
-      <div className="flex flex-col lg:flex-row items-center lg:items-end w-full justify-between">
+      <div className="flex flex-col lg:flex-row items-center lg:items-end w-full justify-between gap-y-5">
         <SeeMoreCard label="Past Winners" link="/winners" />
         <SeeMoreCard label="Past Themes" link="/themes" />
         <SeeMoreCard label="Organisers" link="/organisers" />
